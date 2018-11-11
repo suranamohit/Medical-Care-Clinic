7f3042358cc8fc35a13a3ed4eaefb702816e6e0c
0xPulsar <sooryagangaraj@sooryas-MacBook-Pro.local>
2018-11-11 12:09:32 +0530

    Update src/Doctors/Doctors.js

--- src/Doctors/Doctors.js
+++ src/Doctors/Doctors.js
@@ -38,36 +38,6 @@ class Doctors extends Component {
               />;
             })}
 
-              <Cards
-                name="Dr.Narayan Murthy"
-                experience="Experience:20yrs"
-                degree="MBBS/PHD(Neurology)"
-                imsrc="img/img5.jpg"
-                specialization="NEUROLOGIST"
-                availableTimings="10am to 12pm"
-              />
-            
-            
-              <Cards
-                name="Dr.Keshav Nanda"
-                experience="Experience:12yrs"
-                degree="MBBS"
-                imsrc="img/img6.jpg"
-                specialization="Gynecologist"
-                availableTimings="10am to 12pm"
-              />
-            
-            
-              <Cards
-                name="Johnny Adolf"
-                experience="Experience:7yrs"
-                degree="DDS"
-                imsrc="img/img7.jpg"
-                specialization="Dentist"
-                availableTimings="10am to 12pm"
-              />
-            
-
           </div>
         </div>
       </div>
