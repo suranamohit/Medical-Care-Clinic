42aae15f16358c122f184550f9583ab810a69588
0xpulsar <sooryagangaraj@gmail.com>
2018-11-18 14:45:44 +0530

    Navigation bar is fixed

--- src/Appointment/Appointment.js
+++ src/Appointment/Appointment.js
@@ -8,12 +8,43 @@ class Appointment extends Component {
   render() {
     return (
       <div id="appointment" class="scroll-center container section-height">
+        <br />
+        <br />
+        <br />
         <h3>Appointment</h3>
+        <p>Add all the Conte nts here.</p>
+        {/* starts here */}
+        <div class="Aptab">
+          <button
+            class="tablinks"
+            onclick="openCity(event, 'London')"
+            id="defaultOpen"
+          >
+            London
+          </button>
+          <button class="tablinks" onclick="openCity(event, 'Paris')">
+            Paris
+          </button>
+          <button class="tablinks" onclick="openCity(event, 'Tokyo')">
+            Tokyo
+          </button>
+        </div>
 
-        <p>
-          Medical Care Clinic has emerged as Asia's foremost integrated
-          healthcare services provider and has a robust presence across the
-        </p>
+        <div id="London" class="tabcontent">
+          <h3>London</h3>
+          <p>London is the capital city of England.</p>
+        </div>
+
+        <div id="Paris" class="tabcontent">
+          <h3>Paris</h3>
+          <p>Paris is the capital of France.</p>
+        </div>
+
+        <div id="Tokyo" class="tabcontent">
+          <h3>Tokyo</h3>
+          <p>Tokyo is the capital of Japan.</p>
+        </div>
+        {/* ends here */}
       </div>
     );
   }
